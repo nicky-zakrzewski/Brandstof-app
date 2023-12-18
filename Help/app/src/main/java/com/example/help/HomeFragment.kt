25cc73583f3535e9d1af6964b7242f96b3b39aa3
nicky-zakrzewski <nicky.zakrzewski@student.ucll.be>
2023-12-18 16:24:19 +0100

    Spinner (begin)

--- Help/app/src/main/java/com/example/help/HomeFragment.kt
+++ Help/app/src/main/java/com/example/help/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.example.help
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
+    val fuelTypes = arrayOf("Euro95","Euro98","Diesel","LPG","CNG", "Red Diesel")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -15,7 +18,24 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+
+
+        return view
     }
+
+    //poging spinner
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spinner : Spinner = view.findViewById(R.id.fuelTypeSpinner)
+        val arrayAdapter = ArrayAdapter<String>(requireContext().applicationContext, android.R.layout.simple_spinner_dropdown_item)
+    }
+
+    //einde poging spinner
+
+
 }
